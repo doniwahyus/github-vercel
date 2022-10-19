12d114f20027ab2b69e383fa2456ab9aff3fcbc9
Doni Wahyu S <doniwahyu56@gmail.com>
2022-10-19 20:33:31 +0700

    update data

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-const express = require('express')
-const app = express()
-const port = process.env.port || 3000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const { User } = require('./models');
+const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
 
 app.use(express.json());
-app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-    return res.json({
+    return res.status(200).json({
         status: true,
-        message: "coba docker image sendiri"
-    })
+        message: 'test build docker image sendiri!'
+    });
 });
 
-app.listen(port, () => { console.log("listening on port", port) })
+app.get('/add-data', async (req, res, next) => {
+    try {
+        const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] }); // big_red_donkey
+
+        const user = await User.create({
+            name: randomName
+        });
+
+        return res.status(200).json({
+            status: true,
+            message: 'berhasil',
+            data: user.name
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.get('/get-data', async (req, res, next) => {
+    try {
+        const user = await User.findAll();
+
+        return res.status(200).json({
+            status: true,
+            message: 'berhasil',
+            data: user
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.listen(PORT, () => console.log('listening on port', PORT));
